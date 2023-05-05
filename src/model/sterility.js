f53690dd4e82a6bcfb0eeab3f9481cccc2cac455
bandja047 <francklindassie@gmail.com>
2023-05-05 13:52:59 +0200

    add home page

--- src/model/sterility.js
+++ src/model/sterility.js
@@ -5,11 +5,53 @@ export const sterilitydata = {
             names: "bacteria",
             methodused: {
                 names: "culture on BA/TSA",
-                resultobtained: [
-                    "Growth Obtained",
-                    ""
-                ],
-                referenceValue:"",
+                resultobtained: {
+                    selectedId:1,
+                    options:[
+                        {
+                            id:1,
+                            names:"Growth Obtained"
+                        },
+                        {
+                            id:2,
+                            names:"No Growth Obtained"
+                        },
+                        {
+                            id:3,
+                            names:"Pure colonieis obtained"
+                        },
+                        {
+                            id:4,
+                            names:"Contaminated colonies obtained"
+                        },
+                        {
+                            id:5,
+                            names:"Leave open for number of colonies"
+                        },
+                        {
+                            id:6,
+                            names:"Not Done"
+                        }
+                    ]
+                }
+                   ,
+                referenceValue:{
+                    selectedId:1,
+                    options:[
+                        {
+                            id:1,
+                            names:"Asence of growth"
+                        },
+                        {
+                            id:2,
+                            names:"Presence of pure colonies"
+                        },
+                        {
+                            id:3,
+                            names:"<1 colonie / dose"
+                        }
+                    ]
+                },
                 interpretation: ""
             },
 
